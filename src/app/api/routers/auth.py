1c52ef70b87f2c7cc40b86fee9eaa4c0d4f111d8
the truth <shiftrams@gmail.com>
2025-02-24 23:32:38 -0500

    commit

--- src/app/api/routers/auth.py
+++ src/app/api/routers/auth.py
@@ -1,5 +1,9 @@
 from fastapi.security import OAuth2PasswordRequestForm
-from fastapi import APIRouter, Depends
+from fastapi import (
+    APIRouter,
+    Depends,
+    Header
+)
 from typing import Annotated
 
 from core.security.jwt import OAuthJWTBearer
@@ -17,3 +21,10 @@ async def login_via_credentials(form_data: Annotated[OAuth2PasswordRequestForm, 
     token = OAuthJWTBearer.encode(
         payload={"sub": str(user.get("edbo_id")), "role": user.get("role"), "scope": form_data.scopes or user.get("scopes")})
     return Token(access_token=token)
+
+@router.post("/token", response_model=Token)
+async def auth_token(token: str = Header()):
+    """
+        Login with an access token.
+    """
+    return await OAuthJWTBearer.verify(token=token)
